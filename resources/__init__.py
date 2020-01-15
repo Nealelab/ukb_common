e242ebd6202d042cfefbb3341c6d14353a315725
konradjk <konradjkarczewski@gmail.com>
2020-01-14 19:13:15 -0500

    init fixes

--- resources/__init__.py
+++ resources/__init__.py
@@ -1,1 +1,2 @@
 from ukb_common.resources.generic import *
+from ukb_common.resources.results import *
--- utils/__init__.py
+++ utils/__init__.py
@@ -1,2 +1,3 @@
 from ukb_common.utils.phenotype_loading import *
 from ukb_common.utils.phenotype_munging import *
+from ukb_common.utils.genotype_loading import *
