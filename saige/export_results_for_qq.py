59f3a86f02644aab6f5bf0624d7ebfd478b578c0
konradjk <konradjkarczewski@gmail.com>
2020-01-26 00:48:17 -0500

    add qq plot script

---
+++ saige/export_results_for_qq.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+__author__ = 'konradk'
+
+from ukb_common import *
+import tempfile
+
+
+def main(args):
+    hl.init(master=f'local[{args.n_threads}]',
+            log=hl.utils.timestamp_path(os.path.join(tempfile.gettempdir(), 'export_results_for_qq'), suffix='.log'))
+
+    ht = hl.read_table(args.input_dir + '/variant_results.ht').key_by()
+    ht = ht.select(CHR=ht.locus.contig, BP=ht.locus.position, Pvalue=ht.Pvalue)
+    ht.export(args.output_file)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--input_dir', help='Input directory', required=True)
+    parser.add_argument('--output_file', help='Output file', required=True)
+    parser.add_argument('--n_threads', help='Number of threads to run', type=int, default=8)
+    args = parser.parse_args()
+
+    main(args)
