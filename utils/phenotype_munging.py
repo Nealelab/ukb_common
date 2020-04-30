1c30258eb2191a59b6f1fa893002496c03df4e01
konradjk <konradjkarczewski@gmail.com>
2020-04-30 13:08:59 -0400

    add combine_phenotypes_with_name function

--- utils/phenotype_munging.py
+++ utils/phenotype_munging.py
@@ -56,6 +56,37 @@ def combine_phenotypes(mt: hl.MatrixTable, column_field, entry_field, lists_of_c
     return mt.group_cols_by(new_col_name).aggregate(**{new_entry_name: grouping_function(mt._entry_expr)})
 
 
+def combine_phenotypes_with_name(mt: hl.MatrixTable, column_field, entry_field, dict_of_columns,
+                                 new_col_name='grouping', new_entry_name='new_entry', grouping_function=hl.agg.any):
+    """
+    Group by non-unique fields and apply grouping_function in order to combine entries in MatrixTable.
+
+    Example:
+
+    mt = hl.balding_nichols_model(1, 4, 10)
+    mt = mt.annotate_entries(pheno=hl.rand_bool(0.5))
+    dict_of_columns = {'pheno01': [0, 1], 'pheno03': [0, 3]}
+    entry_field = mt.pheno
+    column_field = mt.sample_idx
+
+    :param MatrixTable mt: Input MatrixTable
+    :param Expression column_field: Column-indexed Expression to group by
+    :param Expression entry_field: Entry-indexed Expression to which to apply `grouping_function`
+    :param dict of any -> list dict_of_columns: Entry in the lists should be the same type as `column_field`
+    :param str new_col_name: Name for new column key (default 'grouping')
+    :param str new_entry_name: Name for new entry expression (default 'new_entry')
+    :param function grouping_function: Aggregator function to apply to `entry_field` (default hl.agg.any)
+    :return: Re-grouped MatrixTable
+    :rtype: MatrixTable
+    """
+    dict_of_columns = hl.literal(dict_of_columns)
+    mt = mt._annotate_all(col_exprs={'_col_expr': column_field}, entry_exprs={'_entry_expr': entry_field})
+    mt = mt.annotate_cols(**{new_col_name: hl.zip(dict_of_columns.keys(), dict_of_columns.values()
+                                          ).filter(lambda x: x[1].contains(mt._col_expr)).map(lambda x: x[0])})
+    mt = mt.explode_cols(new_col_name)
+    return mt.group_cols_by(new_col_name).aggregate(**{new_entry_name: grouping_function(mt._entry_expr)})
+
+
 def conditional_phenotypes(mt: hl.MatrixTable, column_field, entry_field, lists_of_columns,
                            new_col_name='grouping', new_entry_name='new_entry'):
     """
