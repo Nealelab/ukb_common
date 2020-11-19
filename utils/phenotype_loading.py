dab29c932f2cf1803906c938f482045b6e4293c8
wlu04 <wlu04@users.noreply.github.com>
2020-11-19 15:23:14 -0500

    Follow-up changes

--- utils/phenotype_loading.py
+++ utils/phenotype_loading.py
@@ -658,8 +658,8 @@ def load_covid_data(all_samples_ht: hl.Table, covid_data_path: str, hes_main_pat
     #     return f'gs://ukb31063/ukb31063.death.{wave}.txt'
 
     covid_ht = hl.import_table(covid_data_path, delimiter='\t', missing='', impute=True, key='eid')
-    hes_main_ht = hl.import_table(hes_main_path, delimiter='\t', missing='', impute=True, key='eid')
-    hes_diag_ht = hl.import_table(hes_diag_path, delimiter='\t', missing='', impute=True, key='eid')
+    hes_main_ht = hl.import_table(hes_main_path, delimiter='\t', missing='', impute=True, key=('eid', 'ins_index'))
+    hes_diag_ht = hl.import_table(hes_diag_path, delimiter='\t', missing='', impute=True, key=('eid', 'ins_index'))
     death_ht = hl.import_table(death_path, delimiter='\t', missing='', impute=True, key='eid')
 
     death_ht = death_ht.annotate(death_date=hl.experimental.strptime(death_ht.date_of_death + ' 00:00:00', '%d/%m/%Y %H:%M:%S', 'GMT'))  # Add Time Info to Death Register Data
@@ -667,16 +667,16 @@ def load_covid_data(all_samples_ht: hl.Table, covid_data_path: str, hes_main_pat
     hes_main_ht = hes_main_ht.annotate(diag_date=hl.experimental.strptime(hes_main_ht.diagdate + ' 00:00:00', '%d/%m/%Y %H:%M:%S', 'GMT'),
                                        admi_date=hl.experimental.strptime(hes_main_ht.admidate + ' 00:00:00', '%d/%m/%Y %H:%M:%S', 'GMT'))  # Add Time Info to HES Inpatient Data
 
-    hes_ht = hes_main_ht.key_by("eid", "ins_index").join(hes_diag_ht.key_by("eid", "ins_index"))  # Join HES Inpatient Tables
-    hes_pcr_pos = hes_ht.filter(hes_ht.diag_icd10 == "U071")  # Subset Patients with Positive PCR Test Results
-    hes_pcr_pos = hes_pcr_pos.select('diag_date', pcr_result=True)  # Select PCR-Positive Date info
+    hes_ht = hes_main_ht.join(hes_diag_ht)  # Join HES Inpatient Tables
+    hes_pcr_pos = hes_ht.filter(hes_ht.diag_icd10 == 'U071')  # Subset Patients with Positive PCR Test Results
+    hes_pcr_pos = hes_pcr_pos.select(covid_diag_date=hes_pcr_pos.diag_date, pcr_result=True)  # Select PCR-Positive Date info
     hes_ht = hes_ht.select('admi_date', 'diag_icd10')  # Select Admission Date and Diagnoses Info
-    hes_ht = hes_ht.key_by("eid").join(hes_pcr_pos.key_by("eid"), how="outer")  # Create PCR-Positive information for all diagnoses
+    hes_ht = hes_ht.key_by('eid').join(hes_pcr_pos.key_by('eid'), how='outer')  # Create PCR-Positive information for all diagnoses
 
-    hes_death_ht = hes_ht.join(death_ht, how="outer")  # Join Death Register Data to HES Info
-    hes_death_ht = hes_death_ht.annotate(inpatient2=hes_death_ht.admi_date > hes_death_ht.diag_date,
-                                         death=hes_death_ht.death_date > hes_death_ht.diag_date)  # Compare PCR-Positive Date to Death Date and Admission Date
-    hes_death_ht = hes_death_ht.group_by("eid").aggregate(inpatient2=hl.agg.any(hes_death_ht.inpatient2),
+    hes_death_ht = hes_ht.join(death_ht, how='outer')  # Join Death Register Data to HES Info
+    hes_death_ht = hes_death_ht.annotate(inpatient2=hes_death_ht.admi_date >= hes_death_ht.covid_diag_date,
+                                         death=hes_death_ht.death_date >= hes_death_ht.covid_diag_date)  # Compare PCR-Positive Date to Death Date and Admission Date
+    hes_death_ht = hes_death_ht.group_by('eid').aggregate(inpatient2=hl.agg.any(hes_death_ht.inpatient2),
                                                           death=hl.agg.any(hes_death_ht.death),
                                                           pcr_result=hl.agg.any(hes_death_ht.pcr_result),
     )
@@ -686,7 +686,7 @@ def load_covid_data(all_samples_ht: hl.Table, covid_data_path: str, hes_main_pat
         result1=hl.agg.any(covid_ht.result == 1),
         inpatient1=hl.agg.any(covid_ht.reqorg == 1),
     )
-    covid_ht = covid_ht.join(hes_death_ht, how="outer")
+    covid_ht = covid_ht.join(hes_death_ht, how='outer')
     covid_ht = covid_ht.annotate(inpatient=covid_ht.inpatient1 | covid_ht.inpatient2,
                                  result=covid_ht.result1 | covid_ht.pcr_result)
     
